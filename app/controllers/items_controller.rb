19abcf16cb2d89a5741faed29160b09b3f93b3b6
ZUNMASUI <katyou6340@gmail.com>
2021-01-18 18:59:27 +0900

    バリデーションとパラメーターの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,9 @@ class ItemsController < ApplicationController
 
   def new
   end
-  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,:explanation,:category_id,:condition_id,:shipping_fee_id,:shipping_prefecture_id,:shipping_days_id,:price).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_prefecture_id
+    validates :shipping_days_id
+    validates :price
+  end
+  has_one_attached :image
 end
