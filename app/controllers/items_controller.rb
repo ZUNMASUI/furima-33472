05661ddb2ae54bf44c044fde00f9c05d96b5b4fc
ZUNMASUI <katyou6340@gmail.com>
2021-01-20 12:39:54 +0900

    jsファイルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
+
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
+    @item = Item.new
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:title,:explanation,:category_id,:condition_id,:shipping_fee_id,:shipping_prefecture_id,:shipping_days_id,:price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:title,:explanation,:category_id,:condition_id,:shipping_fee_id,:shipping_prefecture_id,:shipping_days_id,:price).merge(user_id:current_user.id)
   end
 end
